1382dee1622b801289a7802f3696454d97df1982
Krishna Prasad <chakravarthy1165@gmail.com>
2024-05-13 18:17:30 +0530

    added for project

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,16 +1,25 @@
-"use client"
+import { apiGet } from '@/app/api/database';
 import withHeader from '@/app/components/withHeader';
 import { CaseStudy } from '@/app/interfaces/common';
 import { axiosl } from '@/app/store/axios';
-import { useParams } from 'next/navigation';
 import React from 'react';
 
-const Project = () => {
-  const params = useParams()
-  const { id } = params;
+export async function generateStaticParams() {
+  let case_studies = await apiGet("SELECT * FROM case_studies", []);
+  return (case_studies as CaseStudy[]).map((case_study) => ({
+    slug: case_study.slug,
+  }))
+}
+
+interface Props {
+  params: { slug: string; };
+}
+
+const Project = ({ params }: Props) => {
+  const { slug } = params
 
   React.useEffect(() => {
-    axiosl.get(`case_studies/${id}`).then(res => {
+    axiosl.get(`case_studies/${slug}`).then(res => {
       const study = (res.data[0] as CaseStudy).body;
       const cont = document.getElementById("main-container");
       if (cont) cont.innerHTML = study;
