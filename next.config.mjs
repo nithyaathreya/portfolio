e2843aae5f0fd0130734c1fac0e6d0321c382b28
nithyaathreya <aathreya28@gmail.com>
2024-05-13 12:04:12 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async exportPathMap(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-    return {
-      '/api/case_studies/[slug]': { page: '/api/case_studies/[slug]' },
-    };
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
